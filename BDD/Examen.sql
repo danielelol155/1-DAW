082a8872fe40d2c76f46607b6490d0203eaf1b3a
danielelol155 <64582361+danielelol155@users.noreply.github.com>
2023-06-06 14:05:37 +0200

    s

--- BDD/Examen.sql
+++ BDD/Examen.sql
@@ -8,10 +8,44 @@ WHERE CodigoJefe = (
     AND Apellido1 = "Soria"
 );
 --4 Nombre, Apellidos y cargo de aquellos empleados que no tienen ningun cliente a su cargo
-SELECT concat(empleados.nombre," ",empleados.apellido1," ",empleados.Apellido2) as "nombre", empleados.cargo as "cargo"
-FROM empleados, clientes
+SELECT concat(empleados.nombre," ",empleados.apellido1," ",empleados.Apellido2) as "nombre", empleados.puesto as "cargo"
+FROM empleados
 WHERE empleados.CodigoEmpleado NOT IN (
-    SELECT empleados.CodigoEmpleado
-    FROM clientes, empleados
-    WHERE empleados.CodigoEmpleado = clientes.CodigoEmpleadoRepVentas
-    );
+    SELECT CodigoEmpleadoRepVentas
+    FROM clientes
+);
+--5. numero de clientes de cada pais
+SELECT COUNT(NombreCliente), pais
+FROM Clientes
+GROUP BY pais;
+--6. Cuantos clientes tienen las ciudades que empiezan por G
+SELECT COUNT(NombreCliente), Ciudad
+FROM Clientes
+WHERE Ciudad LIKE "G%"
+GROUP BY Ciudad;
+--7. Nombre de los empleados junto al nombre de sus jefes
+SELECT CONCAT(E.nombre," ",E.apellido1," ",E.Apellido2) as empleado, CONCAT(J.nombre," ",J.apellido1," ",J.Apellido2) as jefe
+FROM empleados E, empleados J
+WHERE E.CodigoJefe = J.CodigoEmpleado;
+
+--8. Nombre de los clientes a los que no se les ha entregado a tiempo algún pedido(natural join)
+SELECT DISTINCT nombrecliente
+FROM Clientes NATURAL JOIN Pedidos
+WHERE FechaEntrega > FechaEsperada;
+
+
+SELECT nombrecliente
+FROM clientes
+WHERE codigocliente IN(
+    SELECT codigoCliente
+     FROM Pedidos
+    WHERE FechaEntrega > FechaEsperada
+)
+ORDER BY nombrecliente;
+
+
+--9 Listado con los nombres de los clientes y el total pagado por cada uno de ellos
+SELECT clientes.NombreCliente, SUM(pagos.Cantidad)
+FROM clientes, pagos
+WHERE clientes.CodigoCliente = pagos.CodigoCliente
+GROUP BY clientes.NombreCliente;
